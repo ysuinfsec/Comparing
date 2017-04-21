e7f24acc7ede0471be881a78ef62c7d7fd75b416
Roland <roland.yeghiazaryan@gmail.com>
2017-04-21 16:35:39 +0400

    Add project files.

---
+++ Comparing/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing
+{
+    class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + DateOfBirth;
+        }
+
+        public int CompareTo(Person other)
+        {
+            if(this.Name == other.Name)
+                return this.DateOfBirth.CompareTo(other.DateOfBirth);
+            return this.Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Comparing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>()
+            {
+                new Person("John", DateTime.Today.AddYears(-15)),
+                new Person("Alice", DateTime.Today.AddYears(-18)),
+                new Person("Benjamin", DateTime.Today.AddYears(-21)),
+                new Person("Benjamin", DateTime.Today.AddYears(-11))
+            };
+
+            persons.Sort();
+
+            Console.WriteLine(String.Join("\n", persons));
+        }
+    }
+}
